9a65b87b9bcc3dcac677e69eccd351212fbc2346
Ashish Balu <140700105+Ashishbalu@users.noreply.github.com>
2026-03-19 22:29:03 +0530

    updated githubService with fetching logic

--- src/main/java/com/ashish/aiCodeReviewer/githubService/GithubService.java
+++ src/main/java/com/ashish/aiCodeReviewer/githubService/GithubService.java
@@ -4,6 +4,8 @@ import com.ashish.aiCodeReviewer.ai.OllamaClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +27,25 @@ public class GithubService {
         String apiUrl = "https://api.github.com/repos/" + owner + "/" + repo + "/contents";
 
        List<Map<String, Object>> files = restTemplate.getForObject(apiUrl, List.class);
-       return files;
+
+       List<String> reviews = new ArrayList<>();
+
+       for(Map<String, Object> file : files){
+           if ("file".equals(file.get("type"))
+           && file.get("name").toString().endsWith(".java")){
+               String downloadUrl = file.get("download_url").toString();
+
+               String code = restTemplate.getForObject(downloadUrl, String.class);
+
+               //Limited model
+               if (code.length() > 3000){
+                   code = code.substring(0, 3000);
+               }
+               String review = ollamaClient.reviewCode(code);
+
+               reviews.add(review);
+           }
+       }
+       return reviews;
     }
 }
